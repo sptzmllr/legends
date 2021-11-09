98db8ca73a5085de775e86f3092ed0beb285e678
Justus Spitzmueller <jspitzmueller@scilands.de>
2021-11-09 23:54:04 +0100

    Initial commit

---
+++ math_tools.h
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <vector>
+
+namespace lgnds {
+
+namespace math {
+
+template<typename T>
+constexpr T pi = 2 * acos(0.0);
+
+template<typename T>
+constexpr T euler = std::exp(1.0);
+
+template<typename T>
+class statistics
+{
+public:
+	statistics() {};
+	~statistics() {};
+
+	void push_back(T Value)
+	{
+		data_.push_back(Value);
+	}
+
+	void emplace_back(T Value)
+	{
+		data_.emplace_back(Value);
+	}
+
+	void clear()
+	{
+		data_.clear();
+	}
+
+private:
+	std::vector<T>	data_;
+};
+// ended namespace lgnds::math
+};
+
+
+// ended namespace lgnds
+};
---
+++ test_tools/test_math_tools.cpp
@@ -0,0 +1,22 @@
+#include "../math_tools.h"
+#include <iostream>
+
+int main()
+{
+	auto 	pitd = lgnds::math::pi<double>;
+	auto 	pitf = lgnds::math::pi<float>;
+
+	auto	eulerf = lgnds::math::euler<float>;
+	auto	eulerd = lgnds::math::euler<float>;
+
+
+	std::cout << "Pi is : " << pitd << std::endl;
+	std::cout << "Pi is : " << pitf << std::endl;
+	std::cout << "Euler is : " << eulerf << std::endl;
+	std::cout << "Euler is : " << eulerf << std::endl;
+
+	lgnds::math::statistics<double>		Number_Sink;
+
+	Number_Sink.push_back(12.0);
+}
+
